09e695572c870e72733988dfc9d1422a9828207c
Obylisk <obylisk0gmail.com>
2020-04-27 20:05:44 +0700

    Unknown what is wrong with book page but just goes to error

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -9,25 +9,25 @@ var async = require('async');
 
 exports.index = function(req, res) {
 
-    async.parallel({
-        book_count: function(callback) {
-            Book.count(callback);
-        },
-        book_instance_count: function(callback) {
-            BookInstance.count(callback);
-        },
-        book_instance_available_count: function(callback) {
-            BookInstance.count({status:'Available'},callback);
-        },
-        author_count: function(callback) {
-            Author.count(callback);
-        },
-        genre_count: function(callback) {
-            Genre.count(callback);
-        },
-    }, function(err, results) {
-        res.render('index', { title: 'Local Library Home', error: err, data: results });
-    });
+  async.parallel({
+      book_count: function(callback) {
+          Book.countDocuments({}, callback); // Pass an empty object as match condition to find all documents of this collection
+      },
+      book_instance_count: function(callback) {
+          BookInstance.countDocuments({}, callback);
+      },
+      book_instance_available_count: function(callback) {
+          BookInstance.countDocuments({status:'Available'}, callback);
+      },
+      author_count: function(callback) {
+          Author.countDocuments({}, callback);
+      },
+      genre_count: function(callback) {
+          Genre.countDocuments({}, callback);
+      }
+  }, function(err, results) {
+      res.render('index', { title: 'Local Library Home', error: err, data: results });
+  });
 };
 
 // Display list of all books.
