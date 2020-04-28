2a68507ef4b4bf6e235c33e4456c7ca6c2d4e007
Obylisk <obylisk0gmail.com>
2020-04-28 14:31:25 +0700

    Installed compression npm

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 //Import routes for "catalog" area of site
 var catalogRouter = require('./routes/catalog');
-
+var compression = require('compression');
 
 var app = express();
 
@@ -27,6 +27,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(compression()); //Compress all routes
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
